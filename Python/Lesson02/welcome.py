f061c2401b51cbddf3022f14db416db0b4d77b58
cleanixes <harlan.dunlap@gmail.com>
2024-10-15 09:24:57 -0700

    First few lessons

---
+++ Python/Lesson02/welcome.py
@@ -0,0 +1,12 @@
+#expression - 2+2
+#statement - var = x
+line01 = "********************"
+line02 = "*                  *"
+line03 = "*     WELCOME!     *"
+
+print('')
+print(line01)
+print(line02)
+print(line03)
+print(line02)
+print(line01)
