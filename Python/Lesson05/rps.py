e8b598b6e7b0c9b8ee1712f3f74e4e9b3745fb05
cleanixes <harlan.dunlap@gmail.com>
2024-10-15 09:58:21 -0700

    rps

--- Python/Lesson05/rps.py
+++ Python/Lesson05/rps.py
@@ -1,3 +1,20 @@
-value = input('Please enter a value:\n')
+import random
+import sys
+#value = input('Please enter a value:\n')
 
-print(value)
+#print(value)
+
+print("")
+choiceP = input("Enter... \n1 for Rock, \n2 for Paper, \n3 for Scissors:\n\n")
+choice = int(choiceP)
+
+if choice < 1 | choice > 3:
+    sys.exit("invalid")
+
+computerChoice = random.choice("123")
+computer = int(computerChoice)
+
+print("")
+print("You chose " + choice + ".")
+print("Python chose " + computer + ".")
+print("")
